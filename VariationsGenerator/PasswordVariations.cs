6a8c203518ac3ab562b4fb3870b7ede2818f20c4
David <David@c0degeek.dev>
2023-07-26 14:59:42 +0200

    Improve performance in PasswordVariations
    The PasswordVariations class has been enhanced by implementing a caching mechanism. This reduces the computational time for password combinations by storing previous results and using them for future requests if applicable. A HashSet has been used to remove duplicate words before performing computations. Also, the logic for computing permutations has been rewritten to solve a previously unaddressed edge-case.

--- VariationsGenerator/PasswordVariations.cs
+++ VariationsGenerator/PasswordVariations.cs
@@ -1,23 +1,71 @@
-﻿namespace VariationsGenerator;
+﻿using System.Collections.Concurrent;
+
+namespace VariationsGenerator;
 
 public static class PasswordVariations
 {
+    private static readonly ConcurrentDictionary<(List<string>, int), IEnumerable<string>> Cache = new();
+
     public static IEnumerable<string> CombineWords(List<string> words, int maxLength)
     {
-        for (var length = 1; length <= maxLength; length++)
+        // Convert list to HashSet to remove duplicates
+        words = new HashSet<string>(words).ToList();
+
+        if (Cache.TryGetValue((words, maxLength), out var cachedResult))
         {
-            foreach (var combination in GetCombinations(words, length))
+            return cachedResult;
+        }
+
+        var result = ComputeCombinations(words, maxLength).ToList();
+
+        Cache[(words, maxLength)] = result;
+
+        return result;
+    }
+    
+    private static IEnumerable<string> ComputeCombinations(IReadOnlyList<string> words, int maxLength)
+    {
+        var n = words.Count;
+
+        // Run a loop for printing all 2^n subsets one by one
+        for (var i = 0; i < (1<<n); i++)
+        {
+            var combination = new List<string>();
+
+            // Print current subset
+            for (var j = 0; j < n; j++)
             {
-                yield return string.Join(" ", combination);
+                // (1<<j) is a number with jth bit 1, so when we 'and' them with the subset number we get which numbers are present in the subset and which are not
+                if ((i & (1 << j)) > 0 && combination.Count < maxLength)
+                    combination.Add(words[j]);
+            }
+
+            if (combination.Count <= 0 || combination.Count > maxLength) continue;
+            
+            // Generate all permutations of the current combination
+            foreach (var permutation in GeneratePermutations(combination))
+            {
+                yield return string.Join(" ", permutation);
             }
         }
     }
 
-    private static IEnumerable<IEnumerable<T>> GetCombinations<T>(IReadOnlyCollection<T> list, int length)
+
+    private static IEnumerable<IEnumerable<T>> GeneratePermutations<T>(IReadOnlyCollection<T> sequence)
     {
-        if (length == 1) return list.Select(t => new[] { t });
-        return GetCombinations(list, length - 1)
-            .SelectMany(t => list.Where(o => !t.Contains(o)),
-                (t1, t2) => t1.Concat(new[] { t2 }));
+        if (sequence.Count == 1)
+            yield return sequence;
+        else
+        {
+            foreach (var x in sequence)
+            {
+                var rest = sequence.Except(new[] {x}).ToList();
+                var restPermutations = GeneratePermutations(rest);
+                foreach (var restPermutation in restPermutations)
+                {
+                    yield return new[] {x}.Concat(restPermutation);
+                }
+            }
+        }
     }
 }
