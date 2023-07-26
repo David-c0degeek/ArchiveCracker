4918e72c7f7f55607d4af630822c32375b5ecbb6
David <David@c0degeek.dev>
2023-07-26 14:43:34 +0200

    Refactor VariationsGenerator and add new password strategies
    The VariationsGenerator has been refactored to simplify the code structure, improve readability and maintainability. The password strategy list initialization logic has been moved to a new method InitializeStrategies(). ProcessBasePasswords and ProcessBasePassword methods were extracted to handle password processing logic intuitively. The diff also includes addition of new password variation strategies like LeetSpeakStrategy, CommonPrefixesSuffixesStrategy etc., to increase the complexity and coverage of password variations generated, enhancing the effectiveness of the tool.

--- VariationsGenerator/Options.cs
+++ VariationsGenerator/Options.cs
@@ -6,8 +6,10 @@ namespace VariationsGenerator;
 public class Options
 {
     [Option('i', "input", Required = true, HelpText = "Input file containing the passwords.")]
+    // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
     public string InputFile { get; set; } = null!;
 
     [Option('o', "output", Required = false, Default = "output.txt", HelpText = "Output file to write the password variations.")]
+    // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
     public string OutputFile { get; set; } = null!;
 }
--- VariationsGenerator/Program.cs
+++ VariationsGenerator/Program.cs
@@ -4,182 +4,198 @@ using CommandLine;
 using Serilog;
 using VariationsGenerator.Strategies;
 
-namespace VariationsGenerator
+namespace VariationsGenerator;
+
+public static class Program
 {
-    public static class Program
+    public static void Main(string[] args)
     {
-     public static void Main(string[] args)
-        {
-            // Configure logging
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .CreateLogger();
+        // Configure logging
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
 
-            Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(opts =>
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(opts =>
+            {
+                try
                 {
-                    try
-                    {
-                        Log.Information("Starting password variation generation...");
-                        var stopwatch = Stopwatch.StartNew();
+                    Log.Information("Starting password variation generation...");
+                    var stopwatch = Stopwatch.StartNew();
 
-                        ProcessPasswordsAsync(opts).GetAwaiter().GetResult();
+                    ProcessPasswordsAsync(opts).GetAwaiter().GetResult();
 
-                        stopwatch.Stop();
-                        Log.Information("Finished password variation generation in {ElapsedMilliseconds}ms", stopwatch.ElapsedMilliseconds);
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        // Ignore cancellation exceptions
-                        Log.Warning("Operation was cancelled.");
-                    }
-                    catch (AggregateException ae)
-                    {
-                        foreach (var e in ae.Flatten().InnerExceptions)
-                        {
-                            Log.Error(e, "Error occurred during operation");
-                        }
-                    }
-                    catch (Exception e)
+                    stopwatch.Stop();
+                    Log.Information("Finished password variation generation in {ElapsedMilliseconds}ms", stopwatch.ElapsedMilliseconds);
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ignore cancellation exceptions
+                    Log.Warning("Operation was cancelled");
+                }
+                catch (AggregateException ae)
+                {
+                    foreach (var e in ae.Flatten().InnerExceptions)
                     {
                         Log.Error(e, "Error occurred during operation");
                     }
-                });
-
-            Log.CloseAndFlush();
-        }
-
-        private static async Task ProcessPasswordsAsync(Options options)
-        {
-            Log.Information("Initializing password variation strategies...");
-
-            var strategies = new List<IPasswordVariationStrategy>
-            {
-                new SimpleVariationsStrategy(),
-                // Add the rest of the strategies
-            };
-
-            Log.Information("Reading base passwords from input file...");
-
-            var knownPasswords = new HashSet<string>(File.ReadLines(options.InputFile));
-            var variations = new ConcurrentQueue<string>();
-            var cancellationTokenSource = new CancellationTokenSource();
-
-            Log.Information($"Starting to write variations to output file: {options.OutputFile}");
-
-            var writeTask = WriteVariationsToFile(variations, options.OutputFile, cancellationTokenSource.Token);
-
-            Log.Information("Processing base passwords...");
-
-            var tasks = new List<Task>();
-            var degreeOfParallelism = Environment.ProcessorCount;
-            var semaphore = new SemaphoreSlim(degreeOfParallelism);
-            foreach (var basePassword in knownPasswords)
-            {
-                await semaphore.WaitAsync(cancellationTokenSource.Token);
-                tasks.Add(Task.Run(() =>
-                {
-                    try
-                    {
-                        ProcessBasePassword(basePassword, knownPasswords, strategies, variations);
-                    }
-                    catch (Exception e)
-                    {
-                        // Handle exceptions individually and allow other tasks to continue
-                        Log.Error(e, "Error processing password '{BasePassword}'", basePassword);
-                    }
-                    finally
-                    {
-                        semaphore.Release();
-                    }
-                }, cancellationTokenSource.Token));
-            }
-
-            Log.Information("Waiting for all tasks to complete...");
-
-            await Task.WhenAll(tasks);
-            cancellationTokenSource.Cancel();
-
-            Log.Information("All tasks completed. Waiting for write operation to complete...");
-
-            await writeTask;
-
-            Log.Information("All operations completed successfully.");
-        }
-
-        private static void ProcessBasePassword(string basePassword, IEnumerable<string> knownPasswords,
-            IReadOnlyCollection<IPasswordVariationStrategy> strategies, ConcurrentQueue<string> variations)
-        {
-            Log.Information("Processing base password: {BasePassword}", basePassword);
-
-            foreach (var strategy in strategies)
-            {
-                foreach (var variation in strategy.GenerateVariations(basePassword))
-                {
-                    variations.Enqueue(variation);
                 }
-            }
-
-            Log.Information("Finished generating variations for: {BasePassword}", basePassword);
-
-            var otherWords = knownPasswords
-                .Where(p => p != basePassword)
-                .ToList();
-
-            Log.Information("Generating combinations with other words for: {BasePassword}", basePassword);
-
-            var combinations = PasswordVariations.CombineWords(otherWords, maxLength: 3);
-            foreach (var combination in combinations)
-            {
-                var combinedPassword = basePassword + " " + combination;
-                foreach (var variation in GenerateVariations(combinedPassword, strategies))
+                catch (Exception e)
                 {
-                    variations.Enqueue(variation);
+                    Log.Error(e, "Error occurred during operation");
                 }
-            }
+            });
 
-            Log.Information("Finished processing base password: {BasePassword}", basePassword);
+        Log.CloseAndFlush();
+    }
+
+    private static async Task ProcessPasswordsAsync(Options options)
+    {
+        Log.Information("Initializing password variation strategies...");
+        var strategies = InitializeStrategies();
+
+        Log.Information("Reading base passwords from input file...");
+        var knownPasswords = new HashSet<string>(File.ReadLines(options.InputFile));
+
+        var variations = new ConcurrentQueue<string>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        Log.Information("Starting to write variations to output file: {OptionsOutputFile}", options.OutputFile);
+        var writeTask = WriteVariationsToFile(variations, options.OutputFile, cancellationTokenSource.Token);
+
+        Log.Information("Processing base passwords...");
+        await ProcessBasePasswords(knownPasswords, strategies, variations, cancellationTokenSource);
+
+        Log.Information("All tasks completed. Waiting for write operation to complete...");
+        await writeTask;
+
+        Log.Information("All operations completed successfully");
+    }
+
+    private static List<IPasswordVariationStrategy> InitializeStrategies()
+    {
+        return new List<IPasswordVariationStrategy>
+        {
+            new SimpleVariationsStrategy(),
+            new ReversedStrategy(),
+            new CharacterReplacementStrategy(),
+            new AppendNumbersAndSymbolsStrategy(),
+            new LeetSpeakStrategy(),
+            new AppendPrependDatesStrategy(),
+            new AppendPrependCommonSequencesStrategy(),
+            new CapitalizeLettersStrategy(),
+            new CommonPrefixesSuffixesStrategy(),
+            new DoublePasswordStrategy(),
+            new MirrorPasswordStrategy()
+        };
+    }
+
+    private static async Task ProcessBasePasswords(
+        HashSet<string> knownPasswords, 
+        IReadOnlyCollection<IPasswordVariationStrategy> strategies, 
+        ConcurrentQueue<string> variations, 
+        CancellationTokenSource cancellationTokenSource)
+    {
+        var tasks = new List<Task>();
+        var degreeOfParallelism = Environment.ProcessorCount;
+        var semaphore = new SemaphoreSlim(degreeOfParallelism);
+        foreach (var basePassword in knownPasswords)
+        {
+            await semaphore.WaitAsync(cancellationTokenSource.Token);
+            tasks.Add(Task.Run(() =>
+            {
+                try
+                {
+                    ProcessBasePassword(basePassword, knownPasswords, strategies, variations);
+                }
+                catch (Exception e)
+                {
+                    // Handle exceptions individually and allow other tasks to continue
+                    Log.Error(e, "Error processing password '{BasePassword}'", basePassword);
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }, cancellationTokenSource.Token));
         }
 
-        private static async Task WriteVariationsToFile(ConcurrentQueue<string> variations, string filename,
-            CancellationToken cancellationToken)
+        Log.Information("Waiting for all tasks to complete...");
+        await Task.WhenAll(tasks);
+        cancellationTokenSource.Cancel();
+    }
+
+    private static void ProcessBasePassword(string basePassword, IEnumerable<string> knownPasswords,
+        IReadOnlyCollection<IPasswordVariationStrategy> strategies, ConcurrentQueue<string> variations)
+    {
+        Log.Information("Processing base password: {BasePassword}", basePassword);
+
+        foreach (var strategy in strategies)
         {
-            Log.Information("Starting to write variations to file: {Filename}", filename);
+            foreach (var variation in strategy.GenerateVariations(basePassword))
+            {
+                variations.Enqueue(variation);
+            }
+        }
 
-            await using var writer = new StreamWriter(filename);
+        Log.Information("Finished generating variations for: {BasePassword}", basePassword);
 
-            while (true)
+        var otherWords = knownPasswords
+            .Where(p => p != basePassword)
+            .ToList();
+
+        Log.Information("Generating combinations with other words for: {BasePassword}", basePassword);
+
+        var combinations = PasswordVariations.CombineWords(otherWords, maxLength: 3);
+        foreach (var combination in combinations)
+        {
+            var combinedPassword = basePassword + " " + combination;
+            foreach (var variation in GenerateVariations(combinedPassword, strategies))
+            {
+                variations.Enqueue(variation);
+            }
+        }
+
+        Log.Information("Finished processing base password: {BasePassword}", basePassword);
+    }
+
+    private static async Task WriteVariationsToFile(ConcurrentQueue<string> variations, string filename,
+        CancellationToken cancellationToken)
+    {
+        Log.Information("Starting to write variations to file: {Filename}", filename);
+
+        await using var writer = new StreamWriter(filename);
+
+        while (true)
+        {
+            while (variations.TryDequeue(out var variation))
+            {
+                await writer.WriteLineAsync(variation);
+            }
+
+            if (cancellationToken.IsCancellationRequested)
             {
                 while (variations.TryDequeue(out var variation))
                 {
                     await writer.WriteLineAsync(variation);
                 }
 
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    while (variations.TryDequeue(out var variation))
-                    {
-                        await writer.WriteLineAsync(variation);
-                    }
-
-                    break;
-                }
-
-                await Task.Delay(100, cancellationToken);
+                break;
             }
 
-            Log.Information("Finished writing variations to file: {Filename}", filename);
+            await Task.Delay(100, cancellationToken);
         }
+
+        Log.Information("Finished writing variations to file: {Filename}", filename);
+    }
         
-        private static IEnumerable<string> GenerateVariations(string basePassword, IEnumerable<IPasswordVariationStrategy> strategies)
+    private static IEnumerable<string> GenerateVariations(string basePassword, IEnumerable<IPasswordVariationStrategy> strategies)
+    {
+        yield return basePassword;
+
+        foreach (var variation in strategies.SelectMany(strategy => strategy.GenerateVariations(basePassword)))
         {
-            yield return basePassword;
-
-            foreach (var variation in strategies.SelectMany(strategy => strategy.GenerateVariations(basePassword)))
-            {
-                yield return variation;
-            }
+            yield return variation;
         }
-
     }
 }
