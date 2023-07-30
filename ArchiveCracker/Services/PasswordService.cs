63ea7e85b82ddf4c7df1899dc761c0a6bd5140d4
David <David@c0degeek.dev>
2023-07-31 00:24:14 +0200

    Optimize password checking with parallel execution
    Modified the password checking methods in the PasswordService to utilize parallel execution, significantly increasing the speed of password checking. The max degree of parallelism is dynamically determined by the processor count. Also, switched the _commonPasswords list data type to a ReadOnlyCollection for a performance improvement and better protection against modification. These changes will increase the overall performance of the ArchiveCracker software with faster password cracking.

--- ArchiveCracker/Services/PasswordService.cs
+++ ArchiveCracker/Services/PasswordService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
 using ArchiveCracker.Models;
 using ArchiveCracker.Strategies;
 using Serilog;
@@ -8,9 +9,10 @@ namespace ArchiveCracker.Services;
 public class PasswordService
 {
     private readonly FileService _fileService;
-    private readonly List<string> _commonPasswords;
+    private readonly ReadOnlyCollection<string> _commonPasswords;
     private readonly ConcurrentBag<ArchivePasswordPair> _foundPasswords;
     private readonly string _userPasswordsFilePath;
+    private readonly int _maxDegreeOfParallelism;
 
     public PasswordService(ConcurrentBag<ArchivePasswordPair> foundPasswords, string userPasswordsFilePath, FileService fileService, string commonPasswordsFilePath)
     {
@@ -20,29 +22,63 @@ public class PasswordService
 
         // Initialize CommonPasswords list
         _commonPasswords = File.Exists(commonPasswordsFilePath)
-            ? File.ReadAllLines(commonPasswordsFilePath).ToList()
-            : new List<string>();
+            ? new ReadOnlyCollection<string>(File.ReadAllLines(commonPasswordsFilePath).ToList())
+            : new ReadOnlyCollection<string>(new List<string>());
+        
+        // Determine MaxDegreeOfParallelism based on processor count.
+        var processorCount = Environment.ProcessorCount;
+        _maxDegreeOfParallelism = processorCount <= 3 ? 1 : processorCount <= 20 ? processorCount - 2 : processorCount - 4;
     }
         
-    public bool CheckCommonPasswords(IArchiveStrategy strategy, string file)
+public bool CheckCommonPasswords(IArchiveStrategy strategy, string file)
+{
+    var cancellationTokenSource = new CancellationTokenSource();
+    var foundPassword = false;
+
+    try
     {
-        foreach (var password in _commonPasswords.Where(password => strategy.IsPasswordCorrect(file, password)))
+        Parallel.ForEach(_commonPasswords, new ParallelOptions { CancellationToken = cancellationTokenSource.Token , MaxDegreeOfParallelism = _maxDegreeOfParallelism}, (password, state) =>
         {
+            if (cancellationTokenSource.IsCancellationRequested)
+            {
+                state.Stop();
+                return;
+            }
+
+            if (!strategy.IsPasswordCorrect(file, password)) return;
+
             Log.Information("Found password in common passwords for file: {File}", file);
             AddPasswordAndSave(file, password);
-            return true;
-        }
-
-        return false;
+            foundPassword = true;
+            
+            cancellationTokenSource.Cancel();
+            state.Stop();
+        });
+    }
+    catch (OperationCanceledException)
+    {
+        Log.Information("Operation was canceled because a password was found");
     }
 
-    public void CheckUserPasswords(IArchiveStrategy strategy, string file)
-    {
-        using var sr = new StreamReader(_userPasswordsFilePath);
+    return foundPassword;
+}
 
-        while (sr.ReadLine() is { } line)
+public void CheckUserPasswords(IArchiveStrategy strategy, string file)
+{
+    var cancellationTokenSource = new CancellationTokenSource();
+    var userPasswords = File.ReadAllLines(_userPasswordsFilePath);
+
+    try
+    {
+        Parallel.ForEach(userPasswords, new ParallelOptions { CancellationToken = cancellationTokenSource.Token , MaxDegreeOfParallelism = _maxDegreeOfParallelism}, (line, state) =>
         {
-            if (!strategy.IsPasswordCorrect(file, line)) continue;
+            if (cancellationTokenSource.IsCancellationRequested)
+            {
+                state.Stop();
+                return;
+            }
+
+            if (!strategy.IsPasswordCorrect(file, line)) return;
 
             Log.Information("Found password in user passwords for file: {File}", file);
             AddPasswordAndSave(file, line);
@@ -50,14 +86,20 @@ public class PasswordService
             // Check if it's already in common passwords
             if (!_commonPasswords.Contains(line))
             {
-                _commonPasswords.Add(line);
                 _fileService.AppendToCommonPasswordsFile(line);
             }
 
-            break;
-        }
+            cancellationTokenSource.Cancel();
+            state.Stop();
+        });
     }
+    catch (OperationCanceledException)
+    {
+        Log.Information("Operation was canceled because a password was found");
+    }
+}
 
+    
     private void AddPasswordAndSave(string file, string password)
     {
         var archivePasswordPair = new ArchivePasswordPair { File = file, Password = password };
