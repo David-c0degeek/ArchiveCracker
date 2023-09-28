0fdd8b6bffbdc583b1d383592d846513eea46b68
David <David@c0degeek.dev>
2023-09-29 01:23:35 +0200

    Refactor code for C# 10 file-scoped namespace syntax
    The existing explicit namespace declaration syntax has been updated to file-scoped namespace declaration in the Program.cs file. This new syntax introduced in C# 10 simplifies namespace declaration and reduces nesting, enhancing code readability. It does not introduce any change in functionality or behavior.

--- PasswordCollector/Program.cs
+++ PasswordCollector/Program.cs
@@ -2,110 +2,111 @@
 using CommandLine;
 using Serilog;
 
-namespace PasswordCollector
-{
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .WriteTo.File("logfile.log")
-                .CreateLogger();
+namespace PasswordCollector;
 
-            try
-            {
-                Parser.Default.ParseArguments<Options>(args)
-                    .WithParsed(ProcessFiles)
-                    .WithNotParsed(HandleParseError);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "An error occurred while processing files");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("logfile.log")
+            .CreateLogger();
+
+        try
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(ProcessFiles)
+                .WithNotParsed(HandleParseError);
         }
-        
-private static void ProcessFiles(Options opts)
-{
-    // List to hold all text files.
-    var allFiles = Directory.EnumerateFiles(opts.SourceFolder, "*.txt", SearchOption.AllDirectories).ToList();
-
-    // ConcurrentDictionary to hold unique passwords.
-    var uniquePasswords = new ConcurrentDictionary<string, byte>();
-
-    // Counter for tracking progress.
-    var progressCounter = new CountdownEvent(allFiles.Count);
-
-    // Counter for tracking processed passwords.
-    var processedPasswords = 0;
-
-    // Determine MaxDegreeOfParallelism based on processor count.
-    var processorCount = Environment.ProcessorCount;
-    var maxDegreeOfParallelism = processorCount <= 3 ? 1 : processorCount <= 20 ? processorCount - 2 : processorCount - 4;
-
-    // If the output file exists, read existing passwords into the ConcurrentDictionary.
-    if (File.Exists(opts.OutputFile))
-    {
-        foreach (var password in File.ReadLines(opts.OutputFile))
+        catch (Exception ex)
         {
-            uniquePasswords.TryAdd(password, 0);
+            Log.Error(ex, "An error occurred while processing files");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
         }
     }
 
-    // Load all passwords into memory.
-    var allPasswords = allFiles.AsParallel()
-        .WithDegreeOfParallelism(maxDegreeOfParallelism)
-        .SelectMany(file => 
+    private static void ProcessFiles(Options opts)
+    {
+        // List to hold all text files.
+        var allFiles = Directory.EnumerateFiles(opts.SourceFolder, "*.txt", SearchOption.AllDirectories).ToList();
+
+        // ConcurrentDictionary to hold unique passwords.
+        var uniquePasswords = new ConcurrentDictionary<string, byte>();
+
+        // Counter for tracking progress.
+        var progressCounter = new CountdownEvent(allFiles.Count);
+
+        // Counter for tracking processed passwords.
+        var processedPasswords = 0;
+
+        // Determine MaxDegreeOfParallelism based on processor count.
+        var processorCount = Environment.ProcessorCount;
+        var maxDegreeOfParallelism =
+            processorCount <= 3 ? 1 : processorCount <= 20 ? processorCount - 2 : processorCount - 4;
+
+        // If the output file exists, read existing passwords into the ConcurrentDictionary.
+        if (File.Exists(opts.OutputFile))
         {
-            Log.Information("Processing file: {File}", file);
-            var passwords = File.ReadLines(file)
-                .Where(line => !string.IsNullOrWhiteSpace(line))
-                .ToList();
-
-            // Decrement the counter and log the progress after reading each file.
-            progressCounter.Signal();
-            Log.Information("Progress: {Processed}/{Total} files", allFiles.Count - progressCounter.CurrentCount, allFiles.Count);
-
-            return passwords;
-        })
-        .ToList();
-
-    // Process all passwords.
-    allPasswords.AsParallel()
-        .WithDegreeOfParallelism(maxDegreeOfParallelism)
-        .ForAll(password =>
-        {
-            // If the password is unique (not in uniquePasswords), add it to uniquePasswords.
-            if (!uniquePasswords.TryAdd(password, 0)) return;
-            
-            Interlocked.Increment(ref processedPasswords);
-            if (processedPasswords % 10000 == 0)  // Adjust this number based on the desired logging frequency.
+            foreach (var password in File.ReadLines(opts.OutputFile))
             {
-                Log.Information("Progress: {Processed}/{Total} passwords", processedPasswords, allPasswords.Count);
+                uniquePasswords.TryAdd(password, 0);
             }
-        });
+        }
 
-    // Write all unique passwords to the output file.
-    File.WriteAllLines(opts.OutputFile, uniquePasswords.Keys);
-
-    Log.Information("Password collection complete, please check the file {OptsOutputFile}", opts.OutputFile);
-}
-
-
-        private static void HandleParseError(IEnumerable<Error> errs)
-        {
-            foreach (var err in errs)
+        // Load all passwords into memory.
+        var allPasswords = allFiles.AsParallel()
+            .WithDegreeOfParallelism(maxDegreeOfParallelism)
+            .SelectMany(file =>
             {
-                Log.Error("Command line parse Error: {Err}", err.ToString());
-            }
+                Log.Information("Processing file: {File}", file);
+                var passwords = File.ReadLines(file)
+                    .Where(line => !string.IsNullOrWhiteSpace(line))
+                    .ToList();
+
+                // Decrement the counter and log the progress after reading each file.
+                progressCounter.Signal();
+                Log.Information("Progress: {Processed}/{Total} files",
+                    allFiles.Count - progressCounter.CurrentCount, allFiles.Count);
+
+                return passwords;
+            })
+            .ToList();
+
+        // Process all passwords.
+        allPasswords.AsParallel()
+            .WithDegreeOfParallelism(maxDegreeOfParallelism)
+            .ForAll(password =>
+            {
+                // If the password is unique (not in uniquePasswords), add it to uniquePasswords.
+                if (!uniquePasswords.TryAdd(password, 0)) return;
+
+                Interlocked.Increment(ref processedPasswords);
+                if (processedPasswords % 10000 == 0) // Adjust this number based on the desired logging frequency.
+                {
+                    Log.Information("Progress: {Processed}/{Total} passwords", processedPasswords,
+                        allPasswords.Count);
+                }
+            });
+
+        // Write all unique passwords to the output file.
+        File.WriteAllLines(opts.OutputFile, uniquePasswords.Keys);
+
+        Log.Information("Password collection complete, please check the file {OptsOutputFile}", opts.OutputFile);
+    }
+
+
+    private static void HandleParseError(IEnumerable<Error> errs)
+    {
+        foreach (var err in errs)
+        {
+            Log.Error("Command line parse Error: {Err}", err.ToString());
         }
     }
 }
-
 
 
 /*
@@ -125,7 +126,7 @@ private static void ProcessFiles(Options opts)
             var processorCount = Environment.ProcessorCount;
             var maxDegreeOfParallelism = processorCount <= 3 ? 1 : processorCount <= 20 ? processorCount - 2 : processorCount - 4;
 
-            
+
             // If the output file exists, read existing passwords into the ConcurrentDictionary.
             if (File.Exists(opts.OutputFile))
             {
@@ -174,5 +175,5 @@ private static void ProcessFiles(Options opts)
 
             Log.Information("Password collection complete, please check the file {OptsOutputFile}", opts.OutputFile);
         }
-        
+
         */
